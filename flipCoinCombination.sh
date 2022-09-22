5850d131c729b164dee9c93b71b8d1cb70464328
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-09-22 19:25:12 +0530

    Added in: Determine the Percentage of Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,3 +5,32 @@ then
 else
     echo "Tails"
 fi
+echo "Enter the no" 
+read no_of_flips
+declare -A singlet_frequency=( 
+
+                            [H]=0
+                            [T]=0
+
+                              )
+for(( flip = 1; flip <= no_of_flips; flip++ ))
+do
+    echo -n "flip $flip "
+    (( toss = RANDOM % 2 ))
+    case $toss in 
+        0)
+            echo "Heads"
+            (( singlet_frequency[H]++ ))
+            ;;
+        1)
+            echo "Tails"
+            (( singlet_frequency[T]++ ))
+    esac
+done
+
+for combination in ${!singlet_frequency[@]}
+do
+    percentage=$(( ${singlet_frequency[$combination]}  * 100 / no_of_flips ))
+    singlet_frequency[$combination]=$percentage
+    echo "percentage of $combination is ${singlet_frequency[$combination]}%"   
+done
